308deba035034505b3fc2ad6d68dae2dec2bf9c7
Eunjue <129129732+Eunjue@users.noreply.github.com>
2023-05-09 01:09:33 +0000

    max

---
+++ misson_max.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main(){
+  int a;
+  int b;
+  printf("Numbers plz\n");
+  scanf("%d", &a);
+  printf("Another one\n");
+  scanf("%d", &b);
+  if(a>b){
+    printf("더 큰 숫자는 %d",a);}
+    
+    else{
+    printf("더 큰 숫자는 %d",b);
+  }
+  }
--- misson_to.c
+++ misson_to.c
@@ -1,22 +1,22 @@
-//90 이상이면 A,80이상이면 B,70이상 C,60이상 D,60이하 F
-#include <stdio.h>
-main(){
-  int x;
-  printf("점수 입력\n");
-  scanf("%d", &x);
-  if(x>90){
-    printf("A입니다.");
-  }
-  else if (x>80){
-    printf("B입니다.");
-  }
-  else if (x>70){
-    printf("C입니다.");
-  }
-  else if (x>60){
-    printf("D입니다.");
-  }
-  else{
-    printf("that's a big F for you.lol");
-  }
-}
+// //90 이상이면 A,80이상이면 B,70이상 C,60이상 D,60이하 F
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("점수 입력\n");
+//   scanf("%d", &x);
+//   if(x>90){
+//     printf("A입니다.");
+//   }
+//   else if (x>80){
+//     printf("B입니다.");
+//   }
+//   else if (x>70){
+//     printf("C입니다.");
+//   }
+//   else if (x>60){
+//     printf("D입니다.");
+//   }
+//   else{
+//     printf("that's a big F for you.lol");
+//   }
+// }
