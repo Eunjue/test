eb6e7d28d5e213819b0dd6cb1497d65978a1191e
Eunjue <129129732+Eunjue@users.noreply.github.com>
2023-05-16 01:12:34 +0000

    while

--- condition_switch.c
+++ condition_switch.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-main(){
-   int x;
-   printf("1:사과, 2:바나나, 3:키위, 4:망고\n");
-   scanf("%d", &x);
+// #include <stdio.h>
+// main(){
+//    int x;
+//    printf("1:사과, 2:바나나, 3:키위, 4:망고\n");
+//    scanf("%d", &x);
   
-   switch(x){
-     case 1:
-       printf("에플다!\n");
-     break;
-     case 2:
-       printf("죠르노 바나나!\n");
-     break;
-     case 3:
-       printf("위키위키!\n");
-     break;
-     case 4:
-       printf("플라망고!\n");
-       break;
-  defalt:
-     printf("음 다 안 좋아하는 군...놀라운데?\n");
-  }
-   printf("-End-");
-}
+//    switch(x){
+//      case 1:
+//        printf("에플다!\n");
+//      break;
+//      case 2:
+//        printf("죠르노 바나나!\n");
+//      break;
+//      case 3:
+//        printf("위키위키!\n");
+//      break;
+//      case 4:
+//        printf("플라망고!\n");
+//        break;
+//   defalt:
+//      printf("음 다 안 좋아하는 군...놀라운데?\n");
+//   }
+//    printf("-End-");
+// }
