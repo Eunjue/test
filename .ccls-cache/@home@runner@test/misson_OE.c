f470273623ce3ee9236c0081c1ab3fac7db7d352
Eunjue <129129732+Eunjue@users.noreply.github.com>
2023-05-23 10:20:13 +0900

    Revert "Test"

--- .ccls-cache/@home@runner@test/misson_OE.c
+++ .ccls-cache/@home@runner@test/misson_OE.c
@@ -1,12 +1,12 @@
-// #include <stdio.h>
-// main(){
-// int a;  //선언
-//   printf("number plz: ");
-//   scanf("%d", &a);  //a에 저장
+#include <stdio.h>
+main(){
+int a;  //선언
+  printf("number plz: ");
+  scanf("%d", &a);  //a에 저장
 
-//   if(a%2==0) {
-//     printf("it's a even number");  //= ->대입
-//   }
-//   else
-//     printf("that's a odd number");
-// }
+  if(a%2==0) {
+    printf("it's a even number");
+  }
+  else
+    printf("that's a odd number");
+}
